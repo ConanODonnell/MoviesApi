37fd16b1b566836d02ad5626915d05d869946a10
ConanODonnell <conanodonnell@gmail.com>
2015-11-28 12:00:46 +0000

    second commit

---
+++ javascript/controller.js
@@ -0,0 +1,71 @@
+angular.module('movieDBControllers',[])
+.controller('MovieListController',function($scope,MovieListService, myMovieConfig) {
+
+   var url = myMovieConfig.moviesEndpoint + '/popular?api_key=' + myMovieConfig.apiKey;
+   $scope.movieList = [];
+   $scope.loading = true;
+   
+   MovieListService.getList(url).then(
+      function(result){
+          $scope.movieList = result.data.results; 
+          $scope.loading = false; 
+      }
+      ).catch(
+      function(error) { 
+          console.log('error', error)
+        });
+})
+
+.controller('MovieTopRatedController',function($scope,
+MovieListService, myMovieConfig) {
+
+   var url = myMovieConfig.moviesEndpoint + '/top_rated?api_key=' + myMovieConfig.apiKey;
+   $scope.movieList = [];
+   $scope.loading = true;
+   
+   MovieListService.getList(url).then(
+      function(result){
+          $scope.movieList = result.data.results; 
+          $scope.loading = false; 
+      }
+      ).catch(
+        function(error) { 
+          console.log('error', error)
+        });
+})
+
+.controller('MovieNowPlayingController',function($scope,
+MovieListService, myMovieConfig) {
+
+   var url = myMovieConfig.moviesEndpoint + '/now_playing?api_key=' + myMovieConfig.apiKey;
+   $scope.movieList = [];
+   $scope.loading = true;
+   
+   MovieListService.getList(url).then(
+      function(result){
+          $scope.movieList = result.data.results; 
+          $scope.loading = false; 
+      }
+      ).catch(
+        function(error) { 
+          console.log('error', error)
+        });
+})
+
+.controller('MovieUpcomingController',function($scope,
+MovieListService, myMovieConfig) {
+
+   var url = myMovieConfig.moviesEndpoint + '/upcoming?api_key=' + myMovieConfig.apiKey;
+   $scope.movieList = [];
+   $scope.loading = true;
+  
+   MovieListService.getList(url).then(
+      function(result){
+          $scope.movieList = result.data.results; 
+          $scope.loading = false; 
+      }
+      ).catch(
+        function(error) { 
+          console.log('error', error)
+        });
+});
